dcaafc112af9fa7b6cbbfa9f54678d3dc52727a7
LinoSun <40516198+LinoSun@users.noreply.github.com>
2018-06-27 20:49:35 +0800

    Update 以图片为例.py

--- 以图片为例.py
+++ 以图片为例.py
@@ -3,12 +3,12 @@ import os
 # 列表接收当前文件夹中文件名
 files = os.listdir('.')
 # 遍历每一个
-for filename in files:
+for fileName in files:
     # 元组接收分离开的文件名以及后缀名
-    portion = os.path.splitext(filename)
+    portion = os.path.splitext(fileName)
     # 如果后缀是.png
     if portion[1] == ".png":
         # 重新组合文件名和后缀名
-        newname = portion[0] + ".jpg"
-        os.rename(filename, newname)
+        newName = portion[0] + ".jpg"
+        os.rename(fileName, newName)
 
